0bc6ace24a7544ceb2491179f47d6beabbec30e7
AletOne <1226628804@qq.com>
2016-03-09 19:51:58 +0800

    Entity

---
+++ src/com/wang/domain/Image.java
@@ -0,0 +1,57 @@
+package com.wang.domain;
+
+public class Image {
+
+	private String imgUrl;
+	private String text;
+	private int height;
+	private int width;
+	private String keywords;
+	private String title;
+	private String pageUrl;
+	
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPageUrl() {
+		return pageUrl;
+	}
+	public void setPageUrl(String pageUrl) {
+		this.pageUrl = pageUrl;
+	}
+	
+	
+}
